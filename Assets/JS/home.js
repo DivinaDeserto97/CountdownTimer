6871d696f9fa45c7e88d4dd287c6a7a380357506
DivinaDeserto97 <tobiaswild@highspeed.ch>
2022-12-06 11:03:02 +0100

    Projekt Kopiert

---
+++ Assets/JS/home.js
@@ -0,0 +1,14 @@
+
+const nowSec = getSeconds();
+const nowMin = getMin();
+const nowHours = getHours();
+const nowDay = getDay();
+
+let sec = 0;
+let min = 0;
+let hours= 0;
+let day= 0;
+
+function addSec (){
+    
+}
